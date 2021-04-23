7d3c7e179dbd8d3aa847a9b61d1b340a52aa301d
Luchko Sergey <learnSS85@gmail.com>
2021-04-23 19:32:04 +0300

    blog page done

--- app/js/main.js
+++ app/js/main.js
@@ -18,6 +18,13 @@ $(function () {
 		slidesToScroll: 10
 
 	});
+	$('.article-slider__box').slick({
+		prevArrow: ' <button type="button" class="article-slider__arrow article-slider__arrowleft"> <img src="images/arrow-left.svg" alt="arrow left"></button>',
+		nextArrow: ' <button type="button" class="article-slider__arrow article-slider__arrowright"> <img src="images/arrow-right.svg" alt="arrow right"></button>',
+
+
+
+	});
 
 	var mixer = mixitup('.gallery__inner', {
 		load: {
